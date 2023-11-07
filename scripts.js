aac9ec060c8317374a4c1dae217dcd7cebd82900
ax119 <73091231+ax119@users.noreply.github.com>
2023-11-07 13:41:44 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/"}
+const engine = "https://search.brave.com/search?q={query}"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/{query}",
+  duckduckgo: "https://duckduckgo.com/?q={query}",
+  ecosia: "https://www.ecosia.org/search?q={query}",
+  google: "https://www.google.com/search?q={query}",
+  startpage: "https://www.startpage.com/search?q={query}",
+  youtube: "https://www.youtube.com/results?q={query}",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  const url = engineUrls[engine] ?? engine
+  return url.replace("{query}", value)
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"xwZCqwuP3AF7Na9V","label":"frequent","bookmarks":[{"id":"5CT1n2kmLvLWjQl3","label":"MangaDex","url":"https://mangadex.org/titles/follows"},{"id":"ny4whbPVCLBvfi0s","label":"Mosfet","url":"https://mosfet.net"}]},{"id":"4PqRmprt1DKU7GmI","label":"sources","bookmarks":[{"id":"ZyHQWqXgtJSY0NEB","label":"AUR","url":"https://aur.archlinux.org/"},{"id":"eTsahDlHakwB0fwA","label":"ProtonDB","url":"https://protondb.net"},{"id":"gyn7m1kvJ13Qb4fW","label":"r/unixporn","url":"https://reddit.com/r/unixporn/"},{"id":"QFjWYLVfedVMwE92","label":"New bookmark","url":""}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
